a971b0b0bfd1d493fd3530ccb754b9392f90a92e
Théo Le Goc <theo@legoc.net>
2025-04-05 18:13:11 -0400

    Minor update to test release builds

--- src/main.js
+++ src/main.js
@@ -1,109 +1,13 @@
 const {invoke} = window.__TAURI__.core;
 const {getCurrentWindow} = window.__TAURI__.window;
+import {PlayBtn} from "./playBtn.js";
 
 let exitBtn;
 let exitContainer;
 let exitLogoutBtn;
 let exitQuitBtn;
 let playBtn;
-
-class PlayBtn {
-    constructor(btn) {
-        this.btn = btn;
-        this.enabledIntro = btn.querySelector(".lol-play-btn-enabled-intro");
-        this.hoverIntro = btn.querySelector(".lol-play-btn-hover-intro");
-        this.hoverLoop = btn.querySelector(".lol-play-btn-hover-loop");
-        this.hoverOutro = btn.querySelector(".lol-play-btn-hover-outro");
-        this.release = btn.querySelector(".lol-play-btn-release");
-        this.disable(btn.disabled);
-
-        this.onclick = null;
-
-        if (this.enabledIntro) {
-            this.enabledIntro.hidden = false;
-            this.enabledIntro.currentTime = 0;
-            this.enabledIntro.play();
-        }
-
-        btn.addEventListener('mouseenter', () => {
-            if (this.disabled) return;
-            this.hideAll();
-            if (this.hoverIntro) {
-                this.hoverIntro.hidden = false;
-                this.hoverIntro.currentTime = 0;
-                this.hoverIntro.play();
-            }
-            if (this.hoverLoop) {
-                this.hoverLoop.hidden = false;
-                this.hoverLoop.currentTime = 0;
-                this.hoverLoop.play();
-            }
-        });
-
-        btn.addEventListener('mouseleave', () => {
-            if (this.disabled) return;
-            this.hideAll();
-            if (this.hoverOutro) {
-                this.hoverOutro.hidden = false;
-                this.hoverOutro.currentTime = 0;
-                this.hoverOutro.play();
-            }
-        });
-
-        btn.addEventListener('click', () => {
-            if (this.disabled) return;
-            this.hideAll();
-            if (this.release) {
-                this.release.hidden = false;
-                this.release.currentTime = 0;
-                this.release.play();
-            }
-            this.disable(true);
-            if (this.onclick) this.onclick();
-        });
-    }
-
-    addEventListener(event, callback) {
-        this.btn.addEventListener(event, callback);
-    }
-
-    hideAll() {
-        if (this.enabledIntro) {
-            this.enabledIntro.hidden = true;
-        }
-        if (this.hoverIntro) {
-            this.hoverIntro.hidden = true;
-        }
-        if (this.hoverLoop) {
-            this.hoverLoop.hidden = true;
-        }
-        if (this.hoverOutro) {
-            this.hoverOutro.hidden = true;
-        }
-        if (this.release) {
-            this.release.hidden = true;
-        }
-    }
-
-    disable(value) {
-        this.disabled = value;
-        if (this.disabled) {
-            this.hideAll();
-            if (this.release) {
-                this.release.hidden = false;
-                this.release.currentTime = 0;
-                this.release.play();
-            }
-        } else {
-            this.hideAll();
-            if (this.enabledIntro) {
-                this.enabledIntro.hidden = false;
-                this.enabledIntro.currentTime = 0;
-                this.enabledIntro.play();
-            }
-        }
-    }
-}
+let playBtnEl;
 
 const appWindow = getCurrentWindow();
 
@@ -141,5 +45,19 @@ window.addEventListener("DOMContentLoaded", () => {
         appWindow.close();
     });
 
-    playBtn = new PlayBtn(document.querySelector(".lol-play-btn"));
+    playBtnEl = document.querySelector(".lol-play-btn")
+    playBtn = new PlayBtn(playBtnEl);
+
+    playBtnEl.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        console.log("Play button clicked");
+    });
+
+    let playTest = document.querySelector("#play-test");
+    playTest.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        playBtn.disable(false);
+    });
 });
---
+++ src/playBtn.js
@@ -0,0 +1,95 @@
+export class PlayBtn {
+    constructor(btn) {
+        this.btn = btn;
+        this.enabledIntro = btn.querySelector(".lol-play-btn-enabled-intro");
+        this.hoverIntro = btn.querySelector(".lol-play-btn-hover-intro");
+        this.hoverLoop = btn.querySelector(".lol-play-btn-hover-loop");
+        this.hoverOutro = btn.querySelector(".lol-play-btn-hover-outro");
+        this.release = btn.querySelector(".lol-play-btn-release");
+        this.disable(btn.disabled);
+
+        if (this.enabledIntro) {
+            this.enabledIntro.hidden = false;
+            this.enabledIntro.currentTime = 0;
+            this.enabledIntro.play();
+        }
+
+        btn.addEventListener('mouseenter', () => {
+            if (this.disabled) return;
+            this.hideAll();
+            if (this.hoverIntro) {
+                this.hoverIntro.hidden = false;
+                this.hoverIntro.currentTime = 0;
+                this.hoverIntro.play();
+            }
+            if (this.hoverLoop) {
+                this.hoverLoop.hidden = false;
+                this.hoverLoop.currentTime = 0;
+                this.hoverLoop.play();
+            }
+        });
+
+        btn.addEventListener('mouseleave', () => {
+            if (this.disabled) return;
+            this.hideAll();
+            if (this.hoverOutro) {
+                this.hoverOutro.hidden = false;
+                this.hoverOutro.currentTime = 0;
+                this.hoverOutro.play();
+            }
+        });
+
+        btn.addEventListener('click', () => {
+            if (this.disabled) return;
+            this.hideAll();
+            if (this.release) {
+                this.release.hidden = false;
+                this.release.currentTime = 0;
+                this.release.play();
+            }
+            this.disable(true);
+        });
+    }
+
+    addEventListener(event, callback) {
+        this.btn.addEventListener(event, callback);
+    }
+
+    hideAll() {
+        if (this.enabledIntro) {
+            this.enabledIntro.hidden = true;
+        }
+        if (this.hoverIntro) {
+            this.hoverIntro.hidden = true;
+        }
+        if (this.hoverLoop) {
+            this.hoverLoop.hidden = true;
+        }
+        if (this.hoverOutro) {
+            this.hoverOutro.hidden = true;
+        }
+        if (this.release) {
+            this.release.hidden = true;
+        }
+    }
+
+    disable(value) {
+        this.disabled = value;
+        this.btn.disabled = value;
+        if (this.disabled) {
+            this.hideAll();
+            if (this.release) {
+                this.release.hidden = false;
+                this.release.currentTime = 0;
+                this.release.play();
+            }
+        } else {
+            this.hideAll();
+            if (this.enabledIntro) {
+                this.enabledIntro.hidden = false;
+                this.enabledIntro.currentTime = 0;
+                this.enabledIntro.play();
+            }
+        }
+    }
+}
